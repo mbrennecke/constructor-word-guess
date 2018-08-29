36f4aaae8d6bd026eabceb02b45fcff2a696d577
MBrennecke <MBrennecke10@gmail.com>
2018-08-29 10:20:26 -0600

    letter seems to work

---
+++ letter.js
@@ -0,0 +1,21 @@
+
+var Letter = function(guessLetter, checkLetter) {
+	this.guessLetter = guessLetter;
+	this.guessed = false;
+}
+
+Letter.prototype.toString = function() {
+		if (this.guessed) {
+			return this.guessLetter;
+		} else {
+			return "_ ";
+		}
+	};
+	
+Letter.prototype.checkLetter = function(toCheck) {
+	if (toCheck == this.guessLetter) {
+		this.guessed = true;
+	}
+};
+	
+module.exports = Letter;
