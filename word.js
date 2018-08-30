589aac4cc87b0342d8506ceeb89141487d410714
MBrennecke <MBrennecke10@gmail.com>
2018-08-30 13:09:47 -0600

    going in circles

--- word.js
+++ word.js
@@ -2,19 +2,21 @@ var Letter = require("./letter.js");
 
 var Word = function() {
 	this.dispWord = [];
+	this.dispArr = [];
 }
 
 Word.prototype.theWord = function(newWord) {
-		var check = [];
-		this.dispWord = newWord.split("");
-		this.dispWord.forEach(function(item){
-			var l = new Letter(item);
-			check.push(l + '');
-		});
-		this.dispWord = check;
+	wordArr = newWord.split("");
+	for (var i = 0; i<wordArr.length; i++) {
+		this.dispArr.push(new Letter(wordArr[i]));
+		this.dispWord.push(this.dispArr[i] + '');
 	}
+}
 	
 Word.prototype.theChar = function(character) {
+	this.dispArr.forEach(function(item.guessedLetter){
+		
+	})
 	var sendLetter = new Letter(character);
 	console.log(sendLetter);
 	console.log(sendLetter.checkLetter(character));
@@ -22,7 +24,10 @@ Word.prototype.theChar = function(character) {
 
 var w = new Word;
 
+
 w.theWord("test");
+// console.log(w.dispWord.join(""));
+ w.theChar("t");
+// console.log(w.dispWord.join(""));
 console.log(w.dispWord.join(""));
-w.theChar("t");
-console.log(w.dispWord.join(""));
+console.log(w.dispArr);
