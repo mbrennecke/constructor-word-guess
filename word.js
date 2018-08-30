a5865ca82ba994da542e840e2b99b1ca62e47eb6
Matthew Brennecke <mbrennecke10@gmail.com>
2018-08-29 19:54:29 -0600

    word half done

--- word.js
+++ word.js
@@ -2,4 +2,18 @@ var Letter = require("./letter.js");
 
 var Word = function() {
 	this.dispWord = [];
+	this.theWord = function(newWord) {
+		var check = [];
+		this.dispWord = newWord.split("");
+		this.dispWord.forEach(function(item){
+			var l = new Letter(item);
+			check.push(l + '');
+		});
+		this.dispWord = check;
+	}
 }
+
+var w = new Word;
+
+w.theWord("test");
+console.log(w.dispWord.join(""));
