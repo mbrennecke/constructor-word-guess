9e49a0f4b10d783dffe81bb8c2561d1ba94ae644
MBrennecke <MBrennecke10@gmail.com>
2018-09-05 10:04:12 -0600

    adding comments

--- word.js
+++ word.js
@@ -1,16 +1,20 @@
+//Import of letter constructor
 var Letter = require("./letter.js");
 
+//Word constructor
 var Word = function() {
 	this.dispWord = [];
 }
 
+//function to create array to hold the word selected
 Word.prototype.theWord = function(newWord) {
 	var wordArr = newWord.split("");
 	for (var i = 0; i<wordArr.length; i++) {
 		this.dispWord.push(new Letter(wordArr[i]));
 	}
 }
-	
+
+//function to check if the letter selected is in the word	
 Word.prototype.theChar = function(character) {
 	var correctReturn = false;
 	for(var i = 0; i<this.dispWord.length; i++){
